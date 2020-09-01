fa3c4ac5417ec077b649854e9a085be752c69fa0
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-09-01 10:37:23 +0800

    random port for integration test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::net::TcpListener;
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    zero2prod::run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    zero2prod::run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,18 @@
 use reqwest;
+use std::net::TcpListener;
 use tokio;
 use zero2prod;
 
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
+    let url = format!("{}/health_check", &address);
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&url)
         .send()
         .await
         .expect("Failed to execute request.");
@@ -21,7 +23,11 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background ~somehow~
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
