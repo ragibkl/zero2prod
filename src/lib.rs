ebbd1b0bd3a9d86e1512806f7de0e7b7e58d751a
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-10-11 10:48:29 +0800

    initial form read

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
+use serde;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -11,10 +18,16 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+// Let's start simple: we always return a 200 OK
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
