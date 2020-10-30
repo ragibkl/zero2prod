5e4409f54cba63b88c3506033f4c795990614a8c
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-10-30 15:17:31 +0800

    minor refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,16 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
-use serde;
 
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String
-}
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-// Let's start simple: we always return a 200 OK
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+pub mod routes;
 
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(routes::health_check))
+            .route("/subscriptions", web::post().to(routes::subscribe))
+            .route("/", web::get().to(routes::greet))
+            .route("/{name}", web::get().to(routes::greet))
     })
     .listen(listener)?
     .run();
