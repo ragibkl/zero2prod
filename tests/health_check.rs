5c88db1b929ce72c8126c238d8202feb06777de0
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-10-30 18:02:07 +0800

    minor refactor

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,38 +5,27 @@ use tokio;
 use uuid::Uuid;
 use zero2prod;
 
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::run;
+
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
 }
 
-// Launch our application in the background ~somehow~
-async fn spawn_app() -> TestApp {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let address = format!("http://127.0.0.1:{}", port);
-
-    let mut configuration =
-        zero2prod::configuration::get_configuration().expect("Failed to read configuration.");
-
-    configuration.database.database_name = format!(
-        "{}-{}",
-        &configuration.database.database_name,
-        Uuid::new_v4().to_string(),
-    );
-
-    let mut db_conn = PgConnection::connect(&configuration.database.connection_string_without_db())
+async fn config_db(db_config: &DatabaseSettings) -> PgPool {
+    let mut db_conn = PgConnection::connect(&db_config.connection_string_without_db())
         .await
         .expect("Failed to connect to Postgres.");
     db_conn
         .execute(&*format!(
             r#"CREATE DATABASE "{}";"#,
-            configuration.database.database_name
+            db_config.database_name
         ))
         .await
         .expect("Failed to create database.");
 
-    let db_pool = PgPool::connect(&configuration.database.connection_string())
+    let db_pool = PgPool::connect(&db_config.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
@@ -44,7 +33,30 @@ async fn spawn_app() -> TestApp {
         .await
         .expect("Failed to migrate database");
 
-    let server = zero2prod::run(listener, db_pool.clone()).expect("Failed to bind address");
+    return db_pool;
+}
+
+// Launch our application in the background ~somehow~
+async fn spawn_app() -> TestApp {
+    // load app_config
+    let mut app_config = get_configuration().expect("Failed to read configuration.");
+
+    // randomize db_name
+    app_config.database.database_name = format!(
+        "{}-{}",
+        &app_config.database.database_name,
+        Uuid::new_v4().to_string(),
+    );
+
+    // setup tcp listener
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
+
+    // setup test database
+    let db_pool = config_db(&app_config.database).await;
+
+    let server = run(listener, db_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     TestApp { address, db_pool }
