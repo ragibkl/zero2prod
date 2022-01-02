979d24c89d563580868a4bd437c0d6e1c955c0d4
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2022-01-02 18:53:52 +0800

    connect lazy

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,8 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(configuration.database.connection_string().as_str())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPool::connect_lazy(configuration.database.connection_string().as_str())
+        .expect("Failed to create Postgres connection pool.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
